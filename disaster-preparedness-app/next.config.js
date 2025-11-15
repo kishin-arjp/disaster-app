6b4aba57cb522dbc77bf841fcb2baff74269b3f9
kishin-arjp <241024@kindai-toyooka.ed.jp>
2025-11-15 11:30:23 +0900

    Update next.config.js

--- disaster-preparedness-app/next.config.js
+++ disaster-preparedness-app/next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
